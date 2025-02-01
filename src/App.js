b7271605e6bddba871a78539d996ddeb68683a13
alex-zander-0403 <alexzander0403@gmail.com>
2025-02-01 04:27:53 +0300

    get/post axios/fetch, search

--- src/App.js
+++ src/App.js
@@ -2,28 +2,53 @@ import React, { useEffect, useState } from "react";
 import Card from "./components/ui/Card";
 import Header from "./components/ui/Header";
 import SideBar from "./components/ui/SideBar";
+import axios from "axios";
 
 //
-
 function App() {
   //
   const [items, setItems] = useState([]); // состояние массива товаров
   const [cart, setCart] = useState(false); // состояние sideBar корзины
   const [cartItems, setCartItems] = useState([]); // состояние массива товаров в корзине
+  const [searchValue, setSearchValue] = useState(""); // состояние строки поиска
 
   // функция добавления товара в корзину
   const onAddToCart = (newItem) => {
+    axios.post("https://67848ee91ec630ca33a4bf61.mockapi.io/cart", newItem);
     setCartItems((prev) => [...prev, newItem]);
   };
-  // console.log(cartItems);
 
-  // рендер
+  // функция изменения строки поиска
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  // функция обнуления строки поиска (вписал сразу в onClick)
+  // const clearSearchValue = () => {
+  //   setSearchValue("");
+  // };
+
+  // fetch рендер
+  // useEffect(() => {
+  //   fetch("https://67848ee91ec630ca33a4bf61.mockapi.io/items")
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then((json) => setItems(json));
+  // }, []);
+
+  // axios рендер
   useEffect(() => {
-    fetch("https://67848ee91ec630ca33a4bf61.mockapi.io/items")
+    axios
+      .get("https://67848ee91ec630ca33a4bf61.mockapi.io/items")
       .then((res) => {
-        return res.json();
-      })
-      .then((json) => setItems(json));
+        setItems(res.data);
+      });
+    axios
+      .get("https://67848ee91ec630ca33a4bf61.mockapi.io/cart")
+      .then((res) => {
+        setCartItems(res.data);
+      });
   }, []);
 
   //
@@ -34,10 +59,10 @@ function App() {
       )}
 
       <Header openCart={() => setCart(true)} />
-
+      {/* Все товары: */}
       <div className="content">
         <div className="content-top-block">
-          <h1>Все товары:</h1>
+          <h1>{searchValue ? `Поиск по: "${searchValue}"` : "Все товары:"}</h1>
           <div className="search-block">
             <img
               src="/img/magnifying-glass.svg"
@@ -45,14 +70,30 @@ function App() {
               width="20"
               height="20"
             />
-            <input placeholder="поиск..." />
+            <input
+              onChange={onChangeSearchValue}
+              value={searchValue}
+              placeholder="поиск..."
+            />
+            {searchValue && (
+              <img
+                onClick={() => setSearchValue("")}
+                className="clear remove-btn"
+                src="/img/btn-close-active.svg"
+                alt="remove"
+              />
+            )}
           </div>
         </div>
 
         <div className="goods">
-          {items.map((el) => (
-            <Card key={el.id} el={el} onPlus={(obj) => onAddToCart(obj)} />
-          ))}
+          {items
+            .filter((item) =>
+              item.title.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((el) => (
+              <Card key={el.id} el={el} onPlus={(obj) => onAddToCart(obj)} />
+            ))}
         </div>
       </div>
     </div>
